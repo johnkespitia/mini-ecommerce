afe4cf70ac77325ec6ce5f09ba2cfb007570b4b8
Johnk Espitia <jcespitia1@gmail.com>
2021-08-06 06:48:39 -0500

    adapt login

--- app/Views/home/login.php
+++ app/Views/home/login.php
@@ -1,15 +1,14 @@
 <!-- Header -->
-<div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
-  <header>
-    <div></div>
-    <div></div>
-  </header>
+<div class="topbar">
+  <div class="float-left ml-6"><a class="rounded-piece" href="/seller">SELLERS</a></div>
+  <div class="float-right mr-6"><span class="rounded-piece"><?= $_SERVER['REMOTE_ADDR'] ?></span></div>
+</div>
+<div class="header bg-military-image py-7 py-lg-8 pt-lg-9">
   <div class="container">
     <div class="header-body text-center mb-7">
       <div class="row justify-content-center">
         <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-          <h1 class="text-white">Bienvenido!</h1>
-          <p class="text-lead text-white">Debes autenticarte primero para utilizar el CRM.</p>
+          
         </div>
       </div>
     </div>
@@ -23,7 +22,7 @@
             <div class="card-header bg-transparent pb-5">
             <div class="card-body px-lg-5 py-lg-5">
               <div class="text-center text-muted mb-4">
-                <small>Sign in with credentials</small>
+                <h1>Sign in<h1/>
                 <?php
                 if(!empty($errors)){ ?>
                     <div class="alert alert-danger" role="alert">
@@ -31,15 +30,7 @@
                     </div>
                 <?php } ?>
               </div>
-              <form role="form" method="post" action="">
-                <div class="form-group mb-3">
-                  <div class="input-group input-group-merge input-group-alternative">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text" style="background-color: #1e1e2f;"><i class="fas fa-envelope" ></i></span>
-                    </div>
-                    <input class="form-control" placeholder="Email" name="email" style="color: black;" required type="email">
-                  </div>
-                </div>
+              <form role="form" method="post" action="/home/login">
                 <div class="form-group">
                   <div class="input-group input-group-merge input-group-alternative">
                     <div class="input-group-prepend">
@@ -49,7 +40,7 @@
                   </div>
                 </div>
                 <div class="text-center">
-                  <button type="submit" class="btn btn-primary my-4">Sign in</button>
+                  <button type="submit" class="btn btn-success my-4">Sign in</button>
                 </div>
               </form>
             </div>
